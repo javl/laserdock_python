1421d7d7569e830b55d2aea043151c6cdf510d47
javl <mail@jaspervanloenen.com>
2022-11-20 17:46:06 +0100

    switch to f strings

--- laserdock/laser_dock.py
+++ laserdock/laser_dock.py
@@ -17,7 +17,7 @@ def sleep_until(time_to_wake):
     while time.monotonic() < time_to_wake:
         time_to_wait = time_to_wake - time.monotonic()
         sleep_time = min(time_to_wait, 1)
-        # logger.warning('sleeping for %s', sleep_time)
+        # logger.warning(f'sleeping for {sleep_time}')
         time.sleep(min(time_to_wait, 1))  # sleep the minimum of the correct time and 1 second
 
 
@@ -59,7 +59,7 @@ class LaserDock:
                 if dev.is_kernel_driver_active(i):
                     print('detaching kernel driver')
                     dev.detach_kernel_driver(i)
-                print('Interface #%s' % i)
+                print(f'Interface #{i}')
             c += 1
 
         # set the active configuration. With no arguments, the first configuration will be the active one
@@ -69,11 +69,11 @@ class LaserDock:
             return usb.util.endpoint_direction(endpoint.bEndpointAddress) == usb.util.ENDPOINT_OUT
 
         for cfg in dev:
-            print('Configuration #: %s' % str(cfg.bConfigurationValue))
+            print(f'Configuration #: {cfg.bConfigurationValue}')
             for intf in cfg:
-                print('\tInterface,AltInterface: %s,%s' % (str(intf.bInterfaceNumber), str(intf.bAlternateSetting)))
+                print(f'\tInterface: {intf.bInterfaceNumber}, AltInterface: {intf.bAlternateSetting}')
                 for ep in intf:
-                    print('\t\tEndpoint Address: %s' % str(ep.bEndpointAddress))
+                    print(f'\t\tEndpoint Address: {ep.bEndpointAddress}')
 
         # get an endpoint instance
         cfg = dev.get_active_configuration()
@@ -100,7 +100,7 @@ class LaserDock:
             try:
                 self.dev = self.connect()
             except Exception as exc_data:
-                logger.warning('Exception trying to reconnect: %s %s', type(exc_data), exc_data)
+                logger.warning(f'Exception trying to reconnect: {type(exc_data)} {exc_data}')
             else:
                 continue  # or break
             time.sleep(5)
@@ -123,7 +123,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_ENABLE_OUTPUT):
             raise Exception('Bad response')
-        print('Response from enabling output: %s' % response)
+        print(f'Response from enabling output: {response}')
 
     def disable_output(self):
         self.write_ctrl(b'\x80\x00')
@@ -136,7 +136,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_OUTPUT_STATE):
             raise Exception('Bad response')
-        print('Current Output Status: %s' % response)
+        print(f'Current Output Status: {response}')
 
     def get_dac_rate(self):
         """guint32"""
@@ -145,17 +145,17 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_DAC_RATE):
             raise Exception('Bad response')
-        print('Current DAC Rate: %s' % response)
+        print(f'Current DAC Rate: {response}')
 
     def set_dac_rate(self, rate):
         """suint32(d->devh_ctl, 0x82, rate);"""
-        print('Setting DAC Rate to %s' % rate)
+        print(f'Setting DAC Rate to {rate}')
         self.write_ctrl(const.COMMAND_SET_DAC_RATE + struct.pack('<I', rate))
         response = self.read_ctrl()[:4]
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_SET_DAC_RATE):
             raise Exception('Bad response')
-        print('Response after setting DAC rate: %s' % response)
+        print(f'Response after setting DAC rate: {response}')
 
     def get_max_dac_rate(self):
         self.write_ctrl(const.COMMAND_GET_MAX_DAC_RATE)
@@ -163,7 +163,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_MAX_DAC_RATE):
             raise Exception('Bad response')
-        print('Max DAC Rate: %s' % response)
+        print(f'Max DAC Rate: {response}')
 
     def get_min_dac_value(self):
         self.write_ctrl(const.COMMAND_GET_MIN_DAC_VALUE)
@@ -171,7 +171,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_MIN_DAC_VALUE):
             raise Exception('Bad response')
-        print('Min DAC Value: %s' % response)
+        print(f'Min DAC Value: {response}')
 
     def get_max_dac_value(self):
         self.write_ctrl(const.COMMAND_GET_MAX_DAC_VALUE)
@@ -179,7 +179,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_MAX_DAC_VALUE):
             raise Exception('Bad response')
-        print('Max DAC Value: %s' % response)
+        print(f'Max DAC Value: {response}')
 
     def get_sample_element_count(self):
         self.write_ctrl(const.COMMAND_GET_SAMPLE_ELEMENT_COUNT)
@@ -187,7 +187,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_SAMPLE_ELEMENT_COUNT):
             raise Exception('Bad response')
-        print('get_sample_element_count: %s' % response)
+        print(f'get_sample_element_count: {response}')
 
     def get_iso_packet_sample_count(self):
         self.write_ctrl(const.COMMAND_GET_ISO_PACKET_SAMPLE_COUNT)
@@ -195,7 +195,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_ISO_PACKET_SAMPLE_COUNT):
             raise Exception('Bad response')
-        print('get_iso_packet_sample_count: %s' % response)
+        print(f'get_iso_packet_sample_count: {response}')
 
     def get_bulk_packet_sample_count(self):
         self.write_ctrl(const.COMMAND_GET_BULK_PACKET_SAMPLE_COUNT)
@@ -203,7 +203,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_GET_BULK_PACKET_SAMPLE_COUNT):
             raise Exception('Bad response')
-        print('get_bulk_packet_sample_count: %s' % response)
+        print(f'get_bulk_packet_sample_count: {response}')
 
     def get_version_major_number(self):
         """Return the major version number in the firmware"""
@@ -212,7 +212,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_MAJOR_FIRMWARE):
             raise Exception('Bad response')
-        print('Major Firmware Version: %s' % response)
+        print(f'Major Firmware Version: {response}')
 
     def get_version_minor_number(self):
         self.write_ctrl(const.COMMAND_MINOR_FIRMWARE)
@@ -220,7 +220,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_MINOR_FIRMWARE):
             raise Exception('Bad response')
-        print('Minor Firmware Version: %s' % response)
+        print(f'Minor Firmware Version: {response}')
 
     def get_ringbuffer_sample_count(self):
         self.write_ctrl(const.COMMAND_GET_RINGBUFFER_SAMPLE_COUNT)
@@ -238,7 +238,7 @@ class LaserDock:
         command, response = struct.unpack('<HH', response)
         if command != ord(const.COMMAND_SET_RINGBUFFER):
             raise Exception('Bad response')
-        print('Response from set ringbuffer command: %s' % response)
+        print(f'Response from set ringbuffer command: {response}')
 
     def send_samples(self):
         # this one uses the bulk transfer
@@ -265,7 +265,7 @@ class LaserDock:
 
     def burn_sample(self, sample):
         intensity = int(sample['intensity'] * self.intensity_differential * const.FPS + self.intensity_minimum * const.FPS)
-        # logger.warning('intensity is %s, sample is:%s', intensity, sample)
+        # logger.warning(f'intensity is {intensity}, sample is: {sample}')
         for repeat_entry in range(intensity):
             self.packet_samples.append(sample)
             self.potentially_send_samples()
