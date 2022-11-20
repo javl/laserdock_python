bf8b58111af26de1ac6102b58b48132d2f79cae3
javl <mail@jaspervanloenen.com>
2022-11-20 20:20:19 +0100

    simplify

--- draw_circle.py
+++ draw_circle.py
@@ -12,23 +12,20 @@ class CircleBuffer(object):
         self._buffer = self._buffer_elements * [0]
         step_f = 2.0 * pi / circle_steps
 
-        for i in range(circle_steps):
-            x_f = cos(i * step_f)
-            y_f = sin(i * step_f)
-            self._buffer[i * 2] = float_to_laserdock_xy(x_f)
-            self._buffer[i * 2 + 1] = float_to_laserdock_xy(y_f)
-            i += 1
+        for i in range(0, circle_steps, 2):
+            self._buffer[i * 2] = float_to_laserdock_xy(cos(i * step_f))
+            self._buffer[i * 2 + 1] = float_to_laserdock_xy(sin(i * step_f))
 
     def fill_samples(self, samples_per_packet=64):
-        counter = 0
         samples = []
-        while counter < samples_per_packet:
-            samples.append({'x': self._buffer[2 * self._current_position],
-                            'y': self._buffer[2 * self._current_position + 1],
-                            'r': 100,
-                            'g': 0,
-                            'b': 100})
-            counter += 1
+        for counter in range(0, samples_per_packet):
+            samples.append({
+                'x': self._buffer[2 * self._current_position],
+                'y': self._buffer[2 * self._current_position + 1],
+                'r': 100,
+                'g': 0,
+                'b': 100
+            })
             self._current_position += 1
             if self._current_position >= self._circle_steps:
                 self._current_position = 0
